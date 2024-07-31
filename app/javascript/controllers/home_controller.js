8c0fa980fef0fbec06849cf460bc3dd779a00d03
erika fonseca <fonsecarika@gmail.com>
2024-07-31 15:55:14 +0300

    home page index bike done

--- app/javascript/controllers/home_controller.js
+++ app/javascript/controllers/home_controller.js
@@ -1,13 +1,17 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-  static targets = ["frame"]
+  static targets = ["frame", "scroll"]
   connect() {
     // first div frame open up after 5 seconds, and disapear
-     setTimeout(() => {
+    setTimeout(() => {
         // Apply slide-up animation to the frame
-          this.frameTarget.classList.add("frame-slide-up");
-        }, 3000);
+      this.frameTarget.classList.add("frame-slide-up");
+    }, 3000);
+    setTimeout(() => {
+      this.scrollTarget.classList.add("scrollable");
+    }, 3000);
+
     // show the second div home behind to go up
   }
 }
