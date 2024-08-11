8ea9d042154fff399a426cd330bcf8d11bb2fcb4
erika fonseca <fonsecarika@gmail.com>
2024-08-11 23:03:49 +0300

    charge the video before lanidng page

--- app/javascript/controllers/landing_controller.js
+++ app/javascript/controllers/landing_controller.js
@@ -1,12 +1,15 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-  static targets = ["frame"]
+  static targets = ["frame", "content"]
   connect() {
     setTimeout(() => {
         // Apply slide-up animation to the frame
       this.frameTarget.classList.add("frame-slide-up");
     }, 3000);
+    setTimeout(() => {
+      this.contentTarget.classList.remove('hidden-content');
+    }, 3100);
     setTimeout(() => {
        window.location.href = '/bikes';
     }, 3400);
