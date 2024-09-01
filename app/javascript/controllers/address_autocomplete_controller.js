d8215d8fdb4ae2e02e4457dce29f54c44deec07c
erika fonseca <fonsecarika@gmail.com>
2024-09-01 14:32:05 +0300

    some chnagement on the search box for nl

--- app/javascript/controllers/address_autocomplete_controller.js
+++ app/javascript/controllers/address_autocomplete_controller.js
@@ -9,16 +9,20 @@ export default class extends Controller {
   connect() {
     this.geocoder = new MapboxGeocoder({
       accessToken: this.apiKeyValue,
-      types: "country,region,place,postcode,locality,neighborhood,address"
+      placeholder: "Enter a zip code or an address here",
+      bbox: [3.358, 50.750 , 7.227, 53.725],
+      language: "en,nl",
+      types: "region,place,postcode,locality,neighborhood,address",
+      country: "NL",
+      proximity: {
+        longitude: 4.9041,
+        latitude: 52.3676
+      }
     })
+
     this.geocoder.addTo(this.element)
     this.geocoder.on("result", event => this.#setInputValue(event))
     this.geocoder.on("clear", () => this.#clearInputValue())
-
-    const input = this.element.querySelector('.mapboxgl-ctrl-geocoder--input');
-    if (input) {
-      input.placeholder = "Enter a zip code or an address here";
-    }
   }
 
   disconnect() {
