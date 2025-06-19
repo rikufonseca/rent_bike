5f406867405ad09a499dbee2d43f0c55a90a3051
erika fonseca <fonsecarika@gmail.com>
2025-06-19 14:53:41 +0300

    creation de booking done

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -15,4 +15,10 @@ class BookingsController < ApplicationController
       render :new
     end
   end
+
+  def destroy
+    @booking = Booking.find(params[:id])
+    @booking.destroy
+    redirect_to bookings_path
+  end
 end
